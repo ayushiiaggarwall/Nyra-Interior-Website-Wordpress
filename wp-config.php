a4b75fa0bb7b300b51822147c99f88fcb646af6b
Ayushi Aggarwal <ayushiaggarwal1101@gmail.com>
2022-06-29 15:18:25 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'K_Z^wSXpY3N^.vk;8~/}&*AlaqO(58D(EQV?nV/_pxOvR_MJ?0#p*.PY-|fbFYQ1' );
-define( 'SECURE_AUTH_KEY',  '*pH^s;tQPaNlOhcEta;gkC;ZjJ1IK>W]]Vq^w#@/#O$gT_R_<9,@iq1Uyz.AIMAW' );
-define( 'LOGGED_IN_KEY',    'TGXk^(7 PsD~!]9$4o%<!x)yMhl-~^@1nSx&hrjZQ=p;ZMGh<`YmG(?|#65tTV[a' );
-define( 'NONCE_KEY',        'XZ$i$6xS5]cjK]Zct@%I)+`.N1kkQizRVgYt0tkex=s *M ;`ydO_?PBYX}%g>3X' );
-define( 'AUTH_SALT',        '7Nogx-C@lvK(Gy-fSRI~;@-A[PC)Ku.WqcJK-(Wg~|t:!z!Q ?i^31;L!XbHL@O{' );
-define( 'SECURE_AUTH_SALT', 'JT-fU>+$)VXC`eF,:aLnk%WoVA*0&M1@*Ha:TL&CX_Dbjx4wtiZc.3^<lrh0h$>-' );
-define( 'LOGGED_IN_SALT',   '4saHa DD,4%e%,@V!7TD0=a}7]ioie$]Ri-/|a`yB0Z!{H6XSJ+Fj3qF:-B^6p2q' );
-define( 'NONCE_SALT',       'cqrwmT`28LzJm(3J;aJ3P]B]N|[0_1B-b1Z>B8,$.pDp]R`G=HTK]PkN$:yx~u*}' );
+define( 'AUTH_KEY',         '#' );
+define( 'SECURE_AUTH_KEY',  '#' );
+define( 'LOGGED_IN_KEY',    '#' );
+define( 'NONCE_KEY',        '#' );
+define( 'AUTH_SALT',        '#' );
+define( 'SECURE_AUTH_SALT', '#' );
+define( 'LOGGED_IN_SALT',   '#' );
+define( 'NONCE_SALT',       '#' );
 
 /**#@-*/
 
